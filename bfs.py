967f93d154f6629bb5d6f2233a8abecc78d347c5
Shayunak <shayanhamidi2000@gmail.com>
2022-03-06 00:45:47 +0330

    Implemented State evaluation functions

--- bfs.py
+++ bfs.py
@@ -3,6 +3,9 @@ class Point:
         self.Row = Row
         self.Column = Column
     
+    def equals(self, another_point):
+        return self.Row == another_point.Row and self.Column == another_point.Column
+
     def print_out(self):
         print(self.Row, self.Column)
 
@@ -32,9 +35,9 @@ class Base_algorithm:
 
     def create_orc_table(self, orcs):
         table = []
-        for i in range(self.board_dimensions.Row):
+        for i in range(State.board_dimensions.Row):
             new_row = []
-            for j in range(self.board_dimensions.Column):
+            for j in range(State.board_dimensions.Column):
                 new_row.append(None)
             table.append(new_row)
         
@@ -43,7 +46,7 @@ class Base_algorithm:
             orc_rank = orcs[i].Rank
             for j in range(orc_loc.Row - orc_rank, orc_loc.Row + orc_rank + 1):
                 for k in range(orc_loc.Column - orc_rank, orc_loc.Column + orc_rank + 1):
-                    if k >= 0 and k < self.board_dimensions.Column and j >= 0 and j < self.board_dimensions.Row and abs(orc_loc.Row - j) + abs(orc_loc.Column - k) <= orc_rank:
+                    if k >= 0 and k < State.board_dimensions.Column and j >= 0 and j < State.board_dimensions.Row and abs(orc_loc.Row - j) + abs(orc_loc.Column - k) <= orc_rank:
                         table[j][k] = i + 1
             table[orc_loc.Row][orc_loc.Column] = 0
 
@@ -52,30 +55,73 @@ class Base_algorithm:
     def __init__(self, path_to_file):
         self.input_file = open(path_to_file)
         board_dimensions_str = self.read_next_line()
-        self.board_dimensions = Point(int(board_dimensions_str[0]), int(board_dimensions_str[1]))
+        State.board_dimensions = Point(int(board_dimensions_str[0]), int(board_dimensions_str[1]))
         gandalf_position_str = self.read_next_line()
-        self.gandalf_position = Point(int(gandalf_position_str[0]), int(gandalf_position_str[1]))
+        gandalf_position = Point(int(gandalf_position_str[0]), int(gandalf_position_str[1]))
         gondor_position_str = self.read_next_line()
-        self.gondor_position = Point(int(gondor_position_str[0]), int(gondor_position_str[1]))
+        State.gondor_position = Point(int(gondor_position_str[0]), int(gondor_position_str[1]))
         number_of_subsidiary_actors = self.read_next_line()
-        self.number_of_orcs = int(number_of_subsidiary_actors[0])
-        self.number_of_fellowship_members = int(number_of_subsidiary_actors[1])
-        self.Orc_table = self.create_orc_table([self.new_orc(self.read_next_line()) for i in range(self.number_of_orcs)])
-        self.fellowship_members = [self.new_member(self.read_next_line()) for i in range(self.number_of_fellowship_members)]
-        for i in range(len(self.fellowship_members)):
-            self.fellowship_members[i].set_goal(self.read_next_line())
+        number_of_orcs = int(number_of_subsidiary_actors[0])
+        number_of_fellowship_members = int(number_of_subsidiary_actors[1])
+        State.orcs = [self.new_orc(self.read_next_line()) for i in range(number_of_orcs)]
+        State.orc_table = self.create_orc_table(State.orcs)
+        fellowship_members = [self.new_member(self.read_next_line()) for i in range(number_of_fellowship_members)]
+        for i in range(number_of_fellowship_members):
+            fellowship_members[i].set_goal(self.read_next_line())
+        self.initial_state = State(gandalf_position, fellowship_members, cost = 0, in_orc_area = 0, passed_through_orc_cells = 0, accompanying_member = None)
         self.input_file.close()
         
 class State:
-    def __init__(self, gandalf_position, gondor_position, fellowship_members, cost, in_orc_area, passed_through_orc_cells, orc_table):
+    board_dimensions = None
+    orc_table = []
+    orcs = []
+    gondor_position = None
+
+    def __init__(self, gandalf_position, fellowship_members, cost, in_orc_area, passed_through_orc_cells, accompanying_member):
         self.cost = cost
         self.gandalf_position = gandalf_position
-        self.gondor_position = gondor_position
         self.fellowship_members = fellowship_members
         self.in_orc_area = in_orc_area
         self.passed_through_orc_cells = passed_through_orc_cells
-        self.orc_table = orc_table
+        self.accompanying_member = accompanying_member
+
+    def in_bound(self, position):
+        if position.Column >= 0 and position.Column < State.board_dimensions.Column and position.Row >= 0 and position.Row < State.board_dimensions.Row:
+            return True
+        return False
+
+    def is_illegal_state(self):
+        if not self.in_bound(gandalf_position):
+            return True
+        if State.orc_table[gandalf_position.Row][gandalf_position.Column] == 0:
+            return True
+        if State.orcs[in_orc_area - 1].Rank < passed_through_orc_cells:
+            return True
+        return False
+
+    def is_goal_state(self):
+        if not gandalf_position.equals(State.gondor_position):
+            return False
+        for member in self.fellowship_members:
+            if not member.Position.equals(member.Goal):
+                return False
+        
+        return True
+
+    def go_up(self): #Action 1
+        pass
+    
+    def go_left(self): #Action 2
+        pass
+    
+    def go_down(self): #Action 3
+        pass
+    
+    def go_right(self): #Action 4
+        pass
+
+        
 
 alg = Base_algorithm('test_02.txt')
-for row in alg.Orc_table:
-    print(row)  
+for row in State.orc_table:
+    print(row)
